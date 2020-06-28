07f2970444d8f535a7dbac863a27e46e96a696d2
Kushal Agarwalla <kushal.agarwal72@gmail.com>
2020-06-28 17:58:55 +0530

    create and set up session for logout and fix issue

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xf8\xebf3;p\xfb\x03\xeb!K\x7f\x14p\x18\x8c' 
+    # py -c "import os; print(os.urandom(16))" -->run in cmd
     
     MONGODB_SETTINGS= {'db' : 'UTA_Enrollment'}
